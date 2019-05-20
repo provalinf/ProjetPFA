cd957949a7336e2fa17d0dd835014eece87796ed
rsiracus <remi.siracusa@orange.fr>
2019-05-20 10:34:01 +0200

    Ajout des tests unitaires

---
+++ Monnaie/test/company/MachineConversionTest.java
@@ -0,0 +1,15 @@
+package company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MachineConversionTest {
+
+    MachineConversion mc = new MachineConversion();
+
+    @Test
+    void conversion() {
+
+    }
+}
